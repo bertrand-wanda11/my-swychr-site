0fd5c00e4a794a329ba81e7c20f386b97482c29c
unknown <your-github-email@example.com>
2026-04-07 19:48:23 +0100

    finish

--- src/router/views/FrequentView.vue
+++ src/router/views/FrequentView.vue
@@ -223,7 +223,7 @@ const toggleKid = (index) => {
    font-size: 28px;
    font-weight: 600;
    font-weight: 700;
-   padding-left: 10rem;
+   padding-left: 8rem;
    padding-top: 100px;
 }
  </style>
