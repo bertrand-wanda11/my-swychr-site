c77a3367f993e899c33d80a3fb7ce9f188754a85
unknown <your-github-email@example.com>
2026-04-14 11:50:48 +0100

    update

--- src/router/views/SkeletonhomeView.vue
+++ src/router/views/SkeletonhomeView.vue
@@ -697,7 +697,7 @@ the Swychr mobile application, Swychr Connect web platform, and API suites are f
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount, nextTick, watch } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import img1 from '@/assets/images/firstly.png'
 import img2 from '@/assets/images/middle.jpg'
