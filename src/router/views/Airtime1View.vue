0cf3ccd64119487d7191624ec6f93934ea57e486
unknown <your-github-email@example.com>
2026-04-23 10:11:10 +0100

    update

--- src/router/views/Airtime1View.vue
+++ src/router/views/Airtime1View.vue
@@ -520,6 +520,7 @@ onBeforeUnmount(() => {
 }
 
 .topup-card {
+  margin-top: 8rem;
   background: rgba(255, 255, 255, 0.95);
   backdrop-filter: blur(10px);
   border-radius: 24px;
@@ -528,7 +529,7 @@ onBeforeUnmount(() => {
   max-width: 450px;
   color: #333;
   box-shadow: 0 20px 40px rgba(0,0,0,0.2);
-  margin-left: 4rem;
+  margin-left: 2rem;
 }
 
 .card-tagline {
