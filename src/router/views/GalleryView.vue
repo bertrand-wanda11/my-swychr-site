0a6cdc3dbc6bc5c0b47b24697a68a882423ee4b9
unknown <your-github-email@example.com>
2026-05-29 19:40:11 +0100

    grewetata

--- src/router/views/GalleryView.vue
+++ src/router/views/GalleryView.vue
@@ -3,13 +3,17 @@
 <section class="Gallery1">
  <NavBar /> 
 <h1>Welcome To SwyChr's Gallery</h1>  
-<h3>A day at Swychr Cameroon office.</h3>
+
+</section >
+
+<section class= "Gallery2">
+  <h3>A day at Swychr Cameroon office.</h3>
 <a href="https://drive.google.com/drive/folders/1vcgQKDfs4h8mkTHzmkhFvl6XezdWxmZX?usp=sharing" 
 target="_blank"
 rel="noopener noreferrer" class="link-image-wrapper">
   <img src="@/assets/images/SwyChr30.jpg" alt="Clickable Advertisement" class="clickable-img" />
 </a>
-</section >
+</section>
     <FooterView />
   </div>
   </template>
@@ -25,10 +29,16 @@ import FooterView from '@/components/FooterView.vue';
   margin: 0 auto;
   padding: 0.625rem 0;
   height: auto;
+  position: relative; 
   width: 100%;
+  height: 50vh;
+  overflow: hidden;
   align-items: center;
   justify-content: center;
-  background-color: azure;
+  background-image: url('@/assets/images/divi.webp');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 
 .link-image-wrapper {
@@ -43,9 +53,15 @@ import FooterView from '@/components/FooterView.vue';
 }
 
 .clickable-img {
-  width: 100%;
+  width: 75%;
   border-radius: 12px;
   object-fit: cover;
+}
+
+.Gallery2{
+ padding: 5rem 2rem;
+  font-family: 'Montserrat', sans-serif;
+  background-color: #FDFDFD;
 }
 </style>
 
