9873dfdc34f3f48a07df3c2555010ede91d2fe8f
unknown <your-github-email@example.com>
2026-05-29 16:31:57 +0100

    Gallery Update

---
+++ src/router/views/GalleryView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+<section>
+ <NavBar /> 
+ <Marquee><h1>Welcome To SwyChr's Gallery</h1></Marquee>    
+</section>
+    <FooterView />
+  </div>
+  </template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue';
+import FooterView from '@/components/FooterView.vue';   
+</script>
+
+<style scoped>
+
+</style>
+
