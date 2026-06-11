8dc8714f2dc0d0734ae032cd9f557b41bd01d6b9
unknown <your-github-email@example.com>
2026-06-11 14:53:00 +0100

    fix: fotter text

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -120,9 +120,10 @@ the Swychr mobile application, Swychr Connect web platform, and API suites are f
     provided under a Category 1 Microfinance Institution (Swychr Cooperative Society with Board
      of Directors – SWYCO-COOP-BOD), operating  Financial Cooperative <br> Number
      <b>26/022/CMR/SW/53/282/CCA/036004/036004000</b><br>
-<br>
-     © 2026 The Swych Group Inc.(TSG). All rights reserved.<br>
      Swychr Technology Africa Limited is a member of Cameroon Fintech Association (CFIA)
+<br>
+     © 2026 The Swych Group Inc.(TSG). All rights reserved.
+     
     </p>
     </div>
  </section>
