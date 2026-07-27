477b7b102ebe8386d6bacb27e425eac78d34b0ef
unknown <your-github-email@example.com>
2026-07-27 13:54:25 +0100

    fix: add scrollBehavior to router to reset view position to top on navigation

--- src/router/index.js
+++ src/router/index.js
@@ -108,7 +108,14 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0, left: 0 };
+    }
+  }
+});
 
 export default router
